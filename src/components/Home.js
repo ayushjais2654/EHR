7fca17be6a4abb7740ceee1c6707a0eaa8ed0496
ykomal <ykomal678@gmail.com>
2020-02-02 17:55:48 +0530

    home modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,7 +39,7 @@ class Home extends Component {
                     <header>
                         <div class="container">
                             <div class="left">
-                                <a href="#" class="logo">chainCure</a>
+                                <a href="" class="logo" id="homeNav5">chainCure</a>
                                 <div class="hm-menu">
                                     <span></span>
                                     <span></span>
@@ -49,10 +49,10 @@ class Home extends Component {
                             <div class="right">
                                 <nav id = "homeNav">
                                     <ul>
-                                        <li><a href="/">Home</a></li>
-                                        <li><a href="/patientLogin">Patient</a></li>
-                                        <li><a href="/doctorLogin">Doctor</a></li>
-                                        <li><a href="/hospitalLogin">Hospital</a></li>
+                                        <li id= "homeNav1"><a href="/">Home</a></li>
+                                        <li id="homeNav2"><a href="/patientLogin">Patient</a></li>
+                                        <li id="homeNav3"><a href="/doctorLogin">Doctor</a></li>
+                                        <li id="homeNav4"><a href="/hospitalLogin">Hospital</a></li>
                                     </ul>
                                 </nav>
                             </div>
@@ -71,7 +71,7 @@ class Home extends Component {
                 <section id="about">
                     <div class="title">
                         <h2>About Us</h2>
-                        <p>
+                        <p id="about">
                             Health records are classified information about the healthcare of the individuals and is needed to be shared to relevant people such as doctors, pharmacists and researchers . Sharing the  health records sometimes can cause issues which violates the privacy of the patients and hence utmost care should be taken while sharing these documents with others. Blockchain technology has the potential to revolutionize healthcare, placing the patient at the core of the healthcare ecosystem and enhancing the security, privacy, integrity, and interoperability of health data. The data exposed in the healthcare sector is prodigious, ubiquitous, and complicated. The interoperability of this data is a major concern.  Blockchain as a technology provides ways in which the transactions can be monitored and applications can be built with trust, transparency and proper accountability. This can be used to develop a decentralized system to share the EHR data with the hospitals ,  doctors , pharmaceutical companies , researchers and other interested parties. Using blockchain we propose a solution to share , store and manage EHR data. The proposed work aims to leverage the interoperability, integrity, and compliance of blockchain ,thereby, reducing a single point failure in case of centralized databases and providing a greater control over health data to the patients .
               
            </p>
